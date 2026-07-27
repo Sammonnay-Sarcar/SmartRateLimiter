8c4711a32b86c05add54863a17cf6246b6b45eb5
Sammonnay-Sarcar <77871122+Sammonnay-Sarcar@users.noreply.github.com>
2026-07-28 02:57:03 +0530

    Auth complete

--- src/main/java/org/example/smartratelimiter/TestController.java
+++ src/main/java/org/example/smartratelimiter/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("helloworld")
 public class TestController {
     @GetMapping("/")
-    public String getHelloWorld(){
+    public String getHelloWorld(){   
         return "Hello World";
     }
 }
--- src/main/java/org/example/smartratelimiter/auth/SecurityConfig.java
+++ src/main/java/org/example/smartratelimiter/auth/SecurityConfig.java
@@ -6,17 +6,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtService jwtService;    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**","/helloworld/**")
+                        .requestMatchers("/auth/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
@@ -24,7 +29,8 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 );
-
+                http.addFilterBefore(new JwtFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
+                
         return http.build();
     }
 }
